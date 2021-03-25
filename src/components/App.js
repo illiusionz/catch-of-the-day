20b2ef1e31ef9f69b1a51fb479cf59cb629bec4b
Jeffrey Liu <jeffkliu@gmail.com>
2021-03-25 17:05:15 -0400

    Displaying Order State with JSX

--- src/components/App.js
+++ src/components/App.js
@@ -38,19 +38,22 @@ class App extends React.Component {
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
-
                     <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]}  addToOrder={this.addToOrder} />)}
+                        {Object.keys(this.state.fishes).map(key => (
+                        <Fish 
+                            key={key} 
+                            index={key} 
+                            details={this.state.fishes[key]}  
+                            addToOrder={this.addToOrder} 
+                        />
+                        ))}
                     </ul>
                 </div>
-                <Order addToOrder={this.addToOrder}/>
+                <Order fishes={this.state.fishes} order={this.state.order}/>
                 <Inventory 
                     addFish={this.addFish} 
                     loadSampleFishes={this.loadSampleFishes}
                 />
-
-
-
             </div>
         )
     }
